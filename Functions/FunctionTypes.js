9caebfef3e9c749fec537df9b9d5a82d1137ccaf
suresh.gundapu <suresh.gundapu@hiddenbrains.in>
2023-11-27 20:58:04 +0530

    JS New concepts

---
+++ Functions/FunctionTypes.js
@@ -0,0 +1,25 @@
+/**
+ * Types of functions :
+ * ===================
+ * 
+ * 1) named function
+ * 2) anonymous function 
+ * 3) arrow function :
+ * ==================
+  Arrow functions allows a short syntax for writing function expressions.
+
+You don't need the function keyword, the return keyword, and the curly brackets.
+
+Arrow functions do not have their own this. They are not well suited for defining object methods.
+
+Arrow functions are not hoisted. They must be defined before they are used.
+
+Using const is safer than using var, because a function expression is always constant value.
+
+You can only omit the return keyword and the curly brackets if the function is a single statement. Because of this, it might be a good habit to always keep them:
+ * 
+ * 4) IFFEE:
+ * 
+ * 
+ * 
+ */
