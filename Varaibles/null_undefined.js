60090ae13890f7a08ea6688925a239adf0225ddb
suresh.gundapu <suresh.gundapu@hiddenbrains.in>
2023-12-14 20:54:25 +0530

    New Js concepts

--- Varaibles/null_undefined.js
+++ Varaibles/null_undefined.js
@@ -1,15 +1,13 @@
 var a = null;
 var b = undefined;
 
-
 if (a == b) {
   console.log(`this is true`);
-
 } else if (a === b) {
   console.log(`this is false`);
 }
 
-console.log(`the data type of null is `+typeof(null));
-console.log(`the data type of undefined is `+typeof(undefined));
+console.log(`the data type of null is ` + typeof null); //object
+console.log(`the data type of undefined is ` + typeof undefined); //undefined
 
 // null and undefined equally is true means both value is eaqual but when comapre their data type is defferent
